6d985422e28e1e62c5e0d1215cc59fd2ff44433c
npablo1987 <97960344+npablo1987@users.noreply.github.com>
2023-01-10 11:20:32 -0400

    Update settings.py

--- cfttalcaalumnos/settings.py
+++ cfttalcaalumnos/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pvilches1987@gmail.com'
-EMAIL_HOST_PASSWORD = 'jeemjozjfbbhfdhp'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
